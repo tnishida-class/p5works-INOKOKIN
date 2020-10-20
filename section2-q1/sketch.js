cfb06ddca16545f5297ea70bcd693f601b3b8a97
INOKOKIN <72775055+INOKOKIN@users.noreply.github.com>
2020-10-21 00:03:40 +0900

    section2-q1 revised
    this file was revised.
    
    1921061H Koki Inoue

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,15 +1,13 @@
 // 小手調べ
 function setup(){
   noFill();
-  for(let i = 0; i<10 ; i++){
-    let x =i*10
-    ellipse(50,50,x,x);
-    if(i<5){
-      stroke(0,0,255);
-    }
-    else{
+  for(let i = 1; i<=10 ; i++){
+    let x =i*9
+    stroke(0,0,255);
+    if(5<i){
       stroke(255,0,0);
     }
+    ellipse(50,50,x);
     // BLANK[1]
   }
 
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,6 +5,12 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      rect(i*10,j*10,10,10);
+      if(i%2 == 0 ){
+        stroke(150,150,150);
+      }
+      else{
+        stroke(0,0,0);
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
