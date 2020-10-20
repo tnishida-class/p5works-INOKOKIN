0ffa673852ddefacc1d83bdb339477c5110032a8
INOKOKIN <72775055+INOKOKIN@users.noreply.github.com>
2020-10-20 14:50:04 +0900

    section2-q1
    kote-shirabe
    
    1921061H Koki Inoue

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  noFill();
+  for(let i = 0; i<10 ; i++){
+    let x =i*10
+    ellipse(50,50,x,x);
+    if(i<5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
     // BLANK[1]
   }
+
 }
