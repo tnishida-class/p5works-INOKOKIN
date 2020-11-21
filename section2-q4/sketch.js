f6b0a077d84221278afe3f381394467abdfd9762
INOKOKIN <72775055+INOKOKIN@users.noreply.github.com>
2020-11-21 23:58:38 +0900

    section 2-q4
    INOKOKIN
    
    I drew the national flag of Great Britain.

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,37 @@
-// ギリシャ国旗
+// 大英帝国国旗 Great Britain
 function setup() {
-  const blue = color(0, 51, 160);
+  const blue = color(0, 0, 255);
+  const red = color(255, 0, 0);
+  const white = color(255);
   createCanvas(270, 180);
-  // noStroke();
-  background(255);
-
-  let d = height / 9; // 縞1本の太さ
-
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
+  noFill();
+  background(white);
+  let d = height / 6;  // 縞1本の太さ =30
 
   fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
+  rect(0, 0, 270, 180);
 
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  stroke(white);  //stroke(color...)は枠線の色を指定
+  strokeWeight(d);
+  crossmark(0, 0, 270, 180);
+
+  stroke(red);
+　strokeWeight(d/3);
+  crossmark(0, 0, 270, 180);
+
+  stroke(white);  //noStroke();でも良いが後々に響く
+  fill(white);
+  rect(0, 75, 270, 30);
+  rect(120, 0, 30, 180);
+
+  stroke(red);
+  fill(red);
+  rect(0, 83, 270, 15);
+  rect(128, 0, 15, 180);
+
+}
+
+function crossmark(x1, y1, x2, y2){
+  line(x1, y1, x2, y2);
+  line(x2, y1, x1, y2);
 }
